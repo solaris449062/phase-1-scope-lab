ca90b4ebfc40c8532b1ce3d929858d00cfd8d356
Sungoh Park <spark449062@gmail.com>
2021-07-21 12:14:11 -0600

    commit tree 0757bc5d86a8fbca17c0b787df4a9bd7f703d678

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "someone";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "someone else";
+}
+
+
+console.log(customerName);
+// upperCaseCustomerName();
+console.log(customerName);
